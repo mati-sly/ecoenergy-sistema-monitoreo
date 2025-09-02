fd716ef4cbbf004aae3af6e33f9fd398e3b3d6d6
mati-sly <matias34890@gmail.com>
2025-09-02 19:02:37 -0400

    Security: Remove database and use environment variables for credentials

--- monitoreo/monitoreo/settings.py
+++ monitoreo/monitoreo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,8 +121,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'matias34890@gmail.com'
-EMAIL_HOST_PASSWORD = 'ljli lldl mymi sydj'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = 'EcoEnergy <noreply@ecoenergy.com>'
 EMAIL_USE_LOCALTIME = True
 DEFAULT_CHARSET = 'utf-8'
